b90f6915500c2c2cf37b1e22c9c264ddfddfade8
samhoog <102436263+samhoog@users.noreply.github.com>
2022-04-06 13:14:51 -0400

    Add files via upload

---
+++ labs/11/clojure/src/listOps.clj
@@ -0,0 +1,54 @@
+;; listOps.clj is a Clojure "module" for list operations.
+;;
+;; Begun by: Prof. Adams, CS 214 at Calvin College.
+;; Completed by: Sam Hoogewind
+;; Date: March 29, 2022
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; maxOf2() finds the maximum of two values.  
+;; Receive: val1, val2, two values.  
+;; PRE: val1 and val2 can be compared using >.
+;; Return: the maximum of val1 and val2. 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn maxOf2 [val1 val2]
+  (if (> val1 val2)      ; if val1 > val2
+    val1                 ;   return val1
+    val2                 ; else return val2
+  )
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; listMax() finds the maximum value in a list.
+;; Receive: aList, a list of values.    
+;; PRE: aList is a list 
+;;      values in aList can be compared using >.
+;; Return: the maximum value in aList.         
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn listMax [aList]
+  (if (list? aList)
+    (if (empty? aList)
+      nil
+      (let
+        [firstVal (first aList)]
+        (if (= (count aList) 1)
+          firstVal
+          (maxOf2 firstVal (listMax (rest aList)))
+        )
+      )
+    )
+    nil
+  )
+)
+
+;; using Clojure's built-in max and apply functions
+(defn listMax2 [aList]
+  (if (list? aList)       ; if aList is a list:
+    (if (empty? aList)    ;  if aList is empty:
+      nil                 ;    nil
+      (apply max aList)   ;  else apply max to 
+    )                     ;        the items in aList
+  )
+)
+
---
+++ labs/11/lab11-results/script.java
@@ -0,0 +1,79 @@
+Script started on 2022-03-29 13:13:53-04:00 [TERM="xterm-256color" TTY="/dev/pts/1" COLUMNS="189" LINES="12"]
+]0;sth6@gold02: ~/cs/214/labs/11/java[01;32msth6@gold02[00m:[01;34m~/cs/214/labs/11/java[00m$ catr[K Max.java
+/* Max.java finds the maximum values in Java linked lists.
+ *
+ * Begun by: Dr. Adams, CS 214 at Calvin College.
+ * Completed by: Sam Hoogewind
+ * Date: March 29, 2022
+ */
+
+import java.util.LinkedList; 
+import java.util.Iterator; 
+import java.util.Collections;
+
+public class Max {
+
+  public static void main(String [] args) {
+    LinkedList<Integer> list1 = new LinkedList<Integer>();
+    LinkedList<Integer> list2 = new LinkedList<Integer>();
+    LinkedList<Integer> list3 = new LinkedList<Integer>();
+
+    list1.add(99);    // 99, 88, 77, 66, 55
+    list1.add(88);    // max is first
+    list1.add(77);
+    list1.add(66);  
+    list1.add(55);
+
+    list2.add(55);    // 55, 66, 77, 88, 99
+    list2.add(66);    // max is last
+    list2.add(77);  
+    list2.add(88);  
+    list2.add(99);  
+  
+    list3.add(55);    // 55, 77, 99, 88, 66
+    list3.add(77);    // max is in the middle
+    list3.add(99);  
+    list3.add(88);  
+    list3.add(66);  
+  
+    print(list1); 
+    print(list2); 
+    print(list3);
+
+    // display maxima of the 3 lists...
+    System.out.println("Max of list1: " + Collections.max(list1));
+    System.out.println("Max of list2: " + Collections.max(list2));
+    System.out.println("Max of list3: " + Collections.max(list3));
+  }
+
+  /** print() displays a LinkedList on the console.
+   *
+   * @param: aList, a LinkedList<Integer>
+   * 
+   * Postcondition: the Integer values in aList
+   *                 have been displayed to System.out,
+   *                 separated by spaces.
+   */
+  public static void print(LinkedList<Integer> aList)
+  {
+    Iterator<Integer> listIterator = aList.iterator();
+    while (listIterator.hasNext())
+    {
+      System.out.print(listIterator.next() + " ");
+    }
+    System.out.print('\n');
+  }
+
+}
+
+]0;sth6@gold02: ~/cs/214/labs/11/java[01;32msth6@gold02[00m:[01;34m~/cs/214/labs/11/java[00m$ javac-[K -deprecation Max.java
+j]0;sth6@gold02: ~/cs/214/labs/11/java[01;32msth6@gold02[00m:[01;34m~/cs/214/labs/11/java[00m$ java Max
+99 88 77 66 55 
+55 66 77 88 99 
+55 77 99 88 66 
+Max of list1: 99
+Max of list2: 99
+Max of list3: 99
+]0;sth6@gold02: ~/cs/214/labs/11/java[01;32msth6@gold02[00m:[01;34m~/cs/214/labs/11/java[00m$ exit
+
+Script done on 2022-03-29 13:14:04-04:00 [COMMAND_EXIT_CODE="0"]
